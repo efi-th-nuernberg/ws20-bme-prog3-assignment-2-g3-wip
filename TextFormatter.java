95c8e0e097816216e73c19f4337bdff6ee6d5a6a
repl.it user <replituser@example.com>
2020-11-06 11:43:58 +0000

    TextFormatter with new functions

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,7 +1,5 @@
 class TextFormatter {
 
- 
-
   private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
           "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
           "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
@@ -9,6 +7,10 @@ class TextFormatter {
           "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
           "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
           "Lorem ipsum dolor sit amet.";
+
+
+  private String firstPart;
+  private String remainder;
 
   private int maxLineLength;  
 
@@ -19,49 +21,58 @@ class TextFormatter {
 
   }
 
-  // Konstruktor
+
   public TextFormatter(int maxLineLength) {
 
     this.maxLineLength = maxLineLength;
 
   }
-  // Ausgabe
+
+
   public void print(String textA) {
 
-      String firstPart;
-      String remainder;
-   
+    checkLengthAndPrint(textA);
+    System.out.println("Hier sollte der Text mit passendem Umbruch erscheinen.");
+
+  }
+
+
+  public void checkLengthAndPrint(String textA){
+      
       while(textA.length() >= maxLineLength){
 
-        if (textA.charAt(maxLineLength - 1) == ' '){
-          firstPart = textA.substring(0, maxLineLength);
-          remainder = textA.substring(maxLineLength -1);
-          System.out.println(firstPart);
-
-        } else {
-
-          firstPart = textA.substring(0, maxLineLength - 1);
-          remainder = textA.substring(maxLineLength - 1);
-
-          if(textA.charAt(maxLineLength - 2) == ' ')  
-            System.out.println(firstPart );  
-          else
-            System.out.println(firstPart+ '-');
+        if (textA.charAt(maxLineLength - 1) == ' ')
+          printEntireWord(textA);
+         else 
+          printCutOffWord(textA);
           
-        }
-
         if(remainder.charAt(0) == ' ') remainder = remainder.substring(1);
-          
         textA = remainder;
         
       }
 
-      System.out.println(textA);
-      
-   
+        System.out.println(textA);
+
+  }
 
 
-    System.out.println("Hier sollte der Text mit passendem Umbruch erscheinen.");
+  private void printEntireWord(String textA){
+
+    firstPart = textA.substring(0, maxLineLength);
+    remainder = textA.substring(maxLineLength -1);
+    System.out.println(firstPart);
+
+  }
+
+
+  private void printCutOffWord(String textA){
+    firstPart = textA.substring(0, maxLineLength - 1);
+    remainder = textA.substring(maxLineLength - 1);
+
+    if(textA.charAt(maxLineLength - 2) == ' ')  
+       System.out.println(firstPart );  
+     else
+      System.out.println(firstPart+ '-');
   }
 
 }

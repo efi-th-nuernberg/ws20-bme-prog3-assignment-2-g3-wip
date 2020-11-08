135d5969acee6875651894c05a9ad7ff10b3b5a4
repl.it user <replituser@example.com>
2020-11-08 20:34:08 +0000

    TextFormatter (links- und rechtsbündig)

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,7 +1,5 @@
 class TextFormatter {
 
- 
-
   private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
           "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
           "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
@@ -10,58 +8,91 @@ class TextFormatter {
           "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
           "Lorem ipsum dolor sit amet.";
 
+  private StringBuilder builder = new StringBuilder("                              ");
+  int excess;
+  private String firstPart;
+  private String remainder;
+
   private int maxLineLength;  
 
   public static void main(String[] args) {
 
     TextFormatter formatter = new TextFormatter(30);
     formatter.print(text);
+    formatter.alignRight(text);
+    
 
   }
 
-  // Konstruktor
+
   public TextFormatter(int maxLineLength) {
 
     this.maxLineLength = maxLineLength;
 
   }
-  // Ausgabe
+
+
   public void print(String textA) {
+    while(textA.length() >= 30){
 
-      String firstPart;
-      String remainder;
-   
-      while(textA.length() >= maxLineLength){
+      trimSentence(textA);
+      System.out.println(firstPart);
+      textA = remainder;
+    }
 
-        if (textA.charAt(maxLineLength - 1) == ' '){
-          firstPart = textA.substring(0, maxLineLength);
-          remainder = textA.substring(maxLineLength -1);
-          System.out.println(firstPart);
+    System.out.println(remainder + "\n");
 
-        } else {
-
-          firstPart = textA.substring(0, maxLineLength - 1);
-          remainder = textA.substring(maxLineLength - 1);
-
-          if(textA.charAt(maxLineLength - 2) == ' ')  
-            System.out.println(firstPart );  
-          else
-            System.out.println(firstPart+ '-');
-          
-        }
-
-        if(remainder.charAt(0) == ' ') remainder = remainder.substring(1);
-          
-        textA = remainder;
-        
-      }
-
-      System.out.println(textA);
-      
-   
-
-
-    System.out.println("Hier sollte der Text mit passendem Umbruch erscheinen.");
   }
 
+
+     public void alignRight(String textLine) {
+
+      builder.setLength(maxLineLength);
+      if(textLine.charAt(0) == ' ') textLine = textLine.substring(1);
+
+      while(textLine.length() >= maxLineLength){
+        
+        builder.replace(0, 29, "                              ");
+
+        if (textLine.charAt(maxLineLength - 1) == ' '){
+          trimSentence(textLine);
+          System.out.println(firstPart);
+          textLine = remainder;
+        }
+          
+         else{
+           trimSentence(textLine);
+           System.out.println(builder.replace(maxLineLength - firstPart.length(), maxLineLength, firstPart));
+           textLine = remainder;
+         }
+          
+      }
+
+      System.out.println(builder.replace(maxLineLength - remainder.length()-1, maxLineLength, remainder));
+      
+
+   }
+
+  public void trimSentence(String text){
+
+    if (text.charAt(30) == ' '){
+      firstPart = text.substring(1, 30);
+      remainder = text.substring(30);
+
+    } else {
+      
+      while (text.charAt(30 - excess) != ' ') excess++;
+
+      firstPart = text.substring(0, 30-excess+1);
+      remainder = text.substring(30-excess+1);
+      excess = 0;
+
+    }
+  }
+
+
 }
+
+
+
+
